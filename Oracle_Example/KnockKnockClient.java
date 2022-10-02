aca64ea36291a052cc9c127c867416b34ad4edb6
Tikshan Mohan <tik.mohan11@gmail.com>
2022-10-02 10:55:04 +0200

    Oracle Example

---
+++ Oracle_Example/KnockKnockClient.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class KnockKnockClient{
+
+    public static void main(String[] args) throws IOException{
+        String hostName = "localhost";
+        int portNumber = 4200;
+
+        try(
+                Socket kkSocket = new Socket(hostName, portNumber);
+                PrintWriter out = new PrintWriter(kkSocket.getOutputStream(),true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(kkSocket.getInputStream()));
+        ){
+            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+
+            String fromServer;
+            String fromClient;
+
+            while((fromServer = in.readLine()) != null){
+                System.out.println("Server: "+ fromServer);
+                if(fromServer.equals("Bye!")){
+                    break;
+                }
+
+                fromClient = stdIn.readLine();
+                if(fromClient != null){
+                    System.out.println("Client: " + fromClient);
+                    out.println(fromClient);
+                }
+            }
+        }
+            catch(IOException e){
+            System.err.println("IO Problem");
+        }
+    }
+}
---
+++ Oracle_Example/KnockKnockServer.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class KnockKnockServer {
+
+
+    public static void main(String[] args) throws IOException {
+
+        int portNumber = 4200;
+
+        try (
+                ServerSocket serverSocket = new ServerSocket(portNumber);
+                Socket clientSocket = serverSocket.accept();
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        ){
+            String inputLine, outputLine;
+            KnockKnockProtocol kkp = new KnockKnockProtocol();
+            outputLine = kkp.processInput(null);
+            out.println(outputLine);
+
+
+            while((inputLine = in.readLine())!=null){
+                outputLine = kkp.processInput(inputLine);
+                out.println(outputLine);
+                if(outputLine.equals("Bye!"))
+                    break;
+
+            }
+        } catch (IOException e){
+            System.out.println("Exception caught when trying to listen");
+        }
+
+
+    }
+}
